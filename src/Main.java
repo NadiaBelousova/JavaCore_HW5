4d12a496c13cfe74e0ef5573c330d4e35e9a2adb
Надежда <nadin94403@gmail.com>
2023-10-08 19:58:41 +0300

    ДЗ, самый окончательный вариант

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,34 @@
-import exeption.WrongArgumentExeption;
+import exeption.WrongArgumentException;
 import exeption.WrongLoginException;
-import exeption.WrongPasswordExeption;
+import exeption.WrongPasswordException;
+
+import java.util.Objects;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Autorisation autorisation1 = new Autorisation("vasia_78", 1234, 1234);
+        Autorisation autorisation1 = new Autorisation("java_skypro_go", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
         try {
             check(autorisation1);
         } catch (WrongLoginException i) {
             System.out.println("неправильный логин");
-        } catch (WrongPasswordExeption e) {
+        } catch (WrongPasswordException e) {
             System.out.println("неправильный пароль");
-        } catch (WrongArgumentExeption e) {
+        } catch (WrongArgumentException e) {
             System.out.println("введите пароль и логин");
         }
     }
 
     public static final String PATTERN = "^[a-zA-Z0-9-_]+$";
 
-    public static void check(Autorisation autorisation1) throws WrongArgumentExeption, WrongPasswordExeption, WrongLoginException {
+    public static void check(Autorisation autorisation1) throws WrongArgumentException, WrongPasswordException, WrongLoginException {
         if (autorisation1.getLogin().length() > 20 || !autorisation1.getLogin().matches(PATTERN)) {
             throw new WrongLoginException();
-        } else if (autorisation1.getPassword() != autorisation1.getConfirmPass()) {
-            throw new WrongPasswordExeption();
-        } else if (autorisation1.getLogin().isEmpty() || autorisation1.getPassword() == 0 || autorisation1.getConfirmPass() == 0) {
-            throw new WrongArgumentExeption();
+        } else if (!Objects.equals(autorisation1.getPassword(), autorisation1.getConfirmPass()) ||!autorisation1.getPassword().matches(PATTERN)) {
+            throw new WrongPasswordException();
+        } else if (autorisation1.getLogin().isEmpty() || autorisation1.getPassword().isEmpty() || autorisation1.getConfirmPass().isEmpty()) {
+            throw new WrongArgumentException();
         } else {
             System.out.println("вы зашли успешно");
         }
